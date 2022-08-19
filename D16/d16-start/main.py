49832b5d8c8c4d6d17e38bebdc0bd81576a88e1b
jcesco <Juan.C.Escobar.01@gmail.com>
2022-08-18 21:48:39 -0400

    Upload completed assignment for vids 148/149

--- D16/d16-start/main.py
+++ D16/d16-start/main.py
@@ -1,16 +1,33 @@
+#
+#
+# from turtle import Turtle, Screen
+#
+# # Instantantiate object named timmy using Turtle() class in turtle library
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DeepPink2")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
 
-# import turtle
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-# Instantantiate object named timmy using Turtle() class in turtle library
-# timmy = turtle.Turtle()
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DeepPink2")
-timmy.forward(100)
+table = PrettyTable()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Charmander", "Fire"],
+    ]
+)
+
+table.align = "l"
+table.a
+
+print(table)
