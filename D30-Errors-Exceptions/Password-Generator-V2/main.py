9fbf84f63d737ea5adbbe39b02eaee77495d4aeb
jcesco <Juan.C.Escobar.01@gmail.com>
2022-09-26 22:57:53 -0400

    Upload completed V2 of password generator

--- D30-Errors-Exceptions/Password-Generator-V2/main.py
+++ D30-Errors-Exceptions/Password-Generator-V2/main.py
@@ -5,15 +5,8 @@ import pyperclip
 import json
 
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# ------------------------------ FIND PASSWORD ---------------------------------- #
 def find_password():
-    # Search user text entry and see if matches item in json file
-
-    # if yes, show a messagebox with website name and password
-
-    # Catch exception that might occure trying to access the data.json showing messegebox with text "No Dta File Found
-
-    # If users website DNE inside JSON, show messegebox specifying so+
     website = website_entry.get()
 
     try:
@@ -23,14 +16,20 @@ def find_password():
     except FileNotFoundError:
         messagebox.showinfo(title="Error", message="No Data File Found.")
     else:
-        messagebox.showinfo(title=f"{website}", message="")
+        if website in data:
+            messagebox.showinfo(title=f"{website}", message=f"E-mail: {data[website]['email']} \nPassword: "
+                                                            f"{data[website]['password']}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No Data Found For {website}.")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # Password Generator Project
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -45,9 +44,9 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
@@ -91,7 +90,7 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
@@ -99,7 +98,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#Entries
+# Entries
 website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1)
 website_entry.focus()
