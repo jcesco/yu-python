798c0e132e529c92622b27f4a827fb8d6012d9de
jcesco <Juan.C.Escobar.01@gmail.com>
2023-05-15 22:19:30 -0400

    Upload Day 34 Progress

--- D34-GUIQuizApp/QuizlettAppStarter/ui.py
+++ D34-GUIQuizApp/QuizlettAppStarter/ui.py
@@ -1,2 +1,47 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:            #PascalCase
+
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.window = Tk()
+        self.quiz = quiz_brain
+
+        # UI Window
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # Score Board
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        # Question Area
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        # Buttons
+        green_check_image = PhotoImage(file="./images/true.png")
+        self.green_check = Button(image=green_check_image, highlightthickness=0)
+        self.green_check.grid(row=2, column=0)
+        red_x_image = PhotoImage(file="./images/false.png")
+        self.red_x = Button(image=red_x_image, highlightthickness=0)
+        self.red_x.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
