cccf44c0e3ed97e0925e1c7092e843af3237b1e8
jcesco <Juan.C.Escobar.01@gmail.com>
2023-06-13 20:44:18 -0400

    Upload current version of Day 35 OWM OneCall3.0 needs CC info, cannot use. Workaround with CurrentWeather2.5 where possible

--- D35-Authentication-SMS/main.py
+++ D35-Authentication-SMS/main.py
@@ -2,23 +2,22 @@ import requests
 
 # Location Coordinates
 MY_LAT = 26.589180
-MY_LONG = -80.172798
+MY_LON = -80.172798
 
 # OpenWeather API
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-MY_KEY = "f404737043415fdcbdfe549cd4d9200f"
+# OneCall2.5 has been replaced with OneCall3.0 which requires CC info, will suffice with Current Weather Data
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
+MY_KEY = "1a65456994c29f1bf8e3d52af181ae71"
 
 weather_params = {
     "lat": MY_LAT,
-    "lon": MY_LONG,
+    "lon": MY_LON,
     "appid": MY_KEY,
 }
 
-# response = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LONG}&appid={MY_KEY}")
-# print(response)
+# response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={MY_LAT}&lon={MY_LON}&appid={MY_KEY}")
+# print(response.json())
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code)
-
-# response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=London&appid=f404737043415fdcbdfe549cd4d9200f")
-# print(response.json())
+response.raise_for_status()
+weather_data = response.json()
